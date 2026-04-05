31c7b93bb8b86b06746d40de95326f36711052da
Vincze Daniel <vinczedaniel04@gmail.com>
2026-04-05 11:43:55 +0200

    Dockerfile hozzaadasa

--- BackendApi/obj/Debug/net10.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Debug/net10.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf76b90ff96c788a0b1ba38ed07ae226f421fa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
